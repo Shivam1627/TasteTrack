3260c569df8e86ef22bfb5dff71027ceae7e9538
Shivam Singh <166328955+Shivam1627@users.noreply.github.com>
2024-12-27 18:23:21 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://tastetrack-backend.onrender.com'
 
 export const assets ={
     logoo,
@@ -16,4 +17,3 @@ export const assets ={
 }
 
 // export const url = 'http://localhost:4000'
-export const url = 'https://tastetrack-backend.onrender.com'
